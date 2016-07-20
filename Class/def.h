38d81c081c461fdce799d2db7a1cb714caeb2f37
ikuko.masuda <ikuko.masuda@optim.co.jp>
2016-07-20 16:44:33 +0900

    スタティックリンクに変更

--- Class/def.h
+++ Class/def.h
@@ -6,6 +6,9 @@
 #include <vector>
 #include <algorithm>
 
+#include <locale.h>
+#include <locale>
+
 #include <boost/tokenizer.hpp>
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
@@ -25,10 +28,19 @@ typedef boost::wregex regex;
 typedef boost::wsmatch smatch;
 typedef std::wifstream ifstream;
 typedef std::wofstream ofstream;
+# define tout std::wcout
+# define tin std::wcin
+# define terr std::wcerr
+# define tlog std::wclog
 
 #else
 typedef boost::regex regex;
 typedef boost::smatch smatch;
 typedef std::ifstream ifstream;
 typedef std::ofstream ofstream;
+# define tout std::cout
+# define tin std::cin
+# define terr std::cerr
+# define tlog std::clog
+
 #endif // _UNICODE
